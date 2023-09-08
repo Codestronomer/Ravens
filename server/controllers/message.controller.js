2f812d0b07c4e8b8354e33acc4fbe2cea9855335
John Rumide <johnrumide6@gmail.com>
2023-09-08 21:04:04 +0100

    [feat] update messages controller

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -2,17 +2,17 @@ import MessageModel from "../models/message.model.js";
 
 // Create a message
 const createMessage = async (req, res) => {
-  const { senderId, userId, text } = req.body;
+  const { senderId, text, chatId} = req.body;
 
   try {
-    if (!senderId || !userId || !text) {
-      return res.status(400).json({ message: "Bad input: Provide senderId, userId, and text" });
+    if (!senderId || !text || !chatId) {
+      return res.status(400).json({ message: "Bad input: Provide senderId, chatId and text" });
     }
 
-    const newMessage = new MessageModel({ senderId, userId, text });
+    const newMessage = new MessageModel({ senderId, chatId, text });
     const response = await newMessage.save();
 
-    res.status(201).json(response); // Use 201 for resource creation
+    res.status(201).json({ ...response.toObject(), id: response._id }); // Use 201 for resource creation
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
@@ -29,8 +29,11 @@ const getMessages = async (req, res) => {
     }
 
     const messages = await MessageModel.find({ chatId });
+    const response = messages.map((message) => {
+      return { ...message.toObject(), id: message._id }
+    });
 
-    res.status(200).json(messages);
+    res.status(200).json(response);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
